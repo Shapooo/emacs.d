3b58788f549ad038a1bc97e68d6f2d464d633614
Shapo <ShaboJia@gmail.com>
2022-04-19 23:01:39 +0800

    fix desktop

--- lisp/init-sessions.el
+++ lisp/init-sessions.el
@@ -1,9 +1,16 @@
+;;; init-sessions.el --- Initialize desktop. -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Save sessions
+;;
+;;; Code:
 
 (use-package desktop
   :ensure nil
   :init (setq desktop-path (list user-emacs-directory)
               desktop-auto-save-timeout 600
               desktop-load-locked-desktop t)
+  :hook (kill-emacs . (lambda () (desktop-save user-emacs-directory)))
   :config
   (desktop-save-mode 1)
   (setq desktop-globals-to-save
@@ -16,24 +23,17 @@
           (file-name-history        . 100)
           (grep-find-history        . 30)
           (grep-history             . 30)
-          (ido-buffer-history       . 100)
-          (ido-last-directory-list  . 100)
-          (ido-work-directory-list  . 100)
-          (ido-work-file-list       . 100)
           (ivy-history              . 100)
           (magit-read-rev-history   . 50)
-          (minibuffer-history       . 50)
-          (org-clock-history        . 50)
-          (org-refile-history       . 50)
-          (org-tags-history         . 50)
           (query-replace-history    . 60)
           (read-expression-history  . 60)
-          (regexp-history           . 60)
+          (regexp-history           . 20)
           (regexp-search-ring       . 20)
-          ;; register-alist
           (search-ring              . 20)
           (shell-command-history    . 50)
           tags-file-name
-          tags-table-list)))
+          tags-table-list))
+  (setq desktop-restore-frames nil))
 
 (provide 'init-sessions)
+;;; init-sessions.el ends here
